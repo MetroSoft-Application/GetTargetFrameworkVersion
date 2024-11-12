1c5f2b024b2d3efc41c154e434d99ccee7dc459d
MetroSoft-Application <adtd1989@gmail.com>
2024-11-12 23:43:49 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("DLLのパスを指定してください。");
+            return;
+        }
+
+        string dllPath = args[0];
+
+        try
+        {
+            Assembly assembly = Assembly.LoadFrom(dllPath);
+            var targetFrameworkAttribute = assembly.GetCustomAttribute<TargetFrameworkAttribute>();
+
+            if (targetFrameworkAttribute != null)
+            {
+                Console.WriteLine($"ターゲットフレームワーク\t{targetFrameworkAttribute.FrameworkName}");
+            }
+            else
+            {
+                Console.WriteLine("ターゲットフレームワーク情報が見つかりませんでした。");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"エラーが発生しました: {ex.Message}");
+        }
+    }
+}
